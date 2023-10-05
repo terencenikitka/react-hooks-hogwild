bbcf4719477259d2e05cceca3f69b512ed0f7066
Nikita <terencekity@gmail.com>
2023-10-05 11:28:58 -0400

    complete Hogs card

--- src/components/Hogs.js
+++ src/components/Hogs.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import hogs from "../porkers_data";
 
 function Hogs() {
+
+    const [bool,state]=useState(false)
+
     const hogCard = hogs.map((hog) => {
+       function handleClick(){
+state(!bool)
+       }
+
         return (
-        <>
-            <p key={hog.name}>{hog.name}</p>
-            <img key={hog.image} src={hog.image}></img>
-        </>
+        <div onClick={handleClick}>
+            <h3 key={hog.name}>{hog.name}</h3>
+            <img  key={hog.image} src={hog.image}></img>
+            <p >{bool ? 'weight '+hog.weight : ' '} </p>
+            <p >{bool ? hog.specialty : ' '} </p>
+            <p key = {hog.greased}>{bool ? 'is greased? ' + hog.greased : ' '} </p>
+            
+             
+            <p >{bool ? 'Highest medal achieved '+hog['highest medal achieved'] : ' '} </p>
+        </div>
         )
     })
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,9 @@ import React from "react";
 import piggy from "../assets/porco.png";
 
 const Nav = () => {
+	function sortClick(){
+		
+	}
 	return (
 		<div className="navWrapper">
 			<span className="headerText">HogWild</span>
@@ -11,6 +14,7 @@ const Nav = () => {
 			<span className="normalText">
 				A React App for County Fair Hog Fans
 			</span>
+			<button onClick={sortClick}>Click Me! </button>
 		</div>
 	);
 };
